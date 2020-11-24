136403b9c8701564cd43baee1e07281f0a6a66b2
Idris Rupawala <idrisrupawala@gmail.com>
2020-11-24 23:00:21 +0530

    Hibernate User Demo

---
+++ src/main/java/com/rms/startup/DAO/Entities/UserEntity.java
@@ -0,0 +1,53 @@
+package com.rms.startup.DAO.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.rms.startup.Bean.UserBean;
+
+@Entity
+@Table(name="User")
+public class UserEntity {
+	@Id
+	private String mobileNumber;
+	private String password;
+	private int userType;
+	
+	public UserEntity()
+	{}
+	public UserEntity(UserBean u)
+	{
+		this.mobileNumber = u.getMobileNumber();
+		this.password = u.getPassword();
+		this.userType = u.getUserType();
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getUserType() {
+		return userType;
+	}
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	
+	public UserBean convertToBean()
+	{
+		UserBean u = new UserBean();
+		u.setMobileNumber(this.mobileNumber);
+		u.setPassword(this.password);
+		u.setUserType(this.userType);
+		return u;
+	}
+	
+}
--- src/main/java/com/rms/startup/DAO/UserDAO.java
+++ src/main/java/com/rms/startup/DAO/UserDAO.java
@@ -1,13 +1,19 @@
 package com.rms.startup.DAO;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.rms.startup.Bean.UserBean;
+import com.rms.startup.DAO.Entities.UserEntity;
 
 @Repository
 public class UserDAO {
@@ -15,13 +21,32 @@ public class UserDAO {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	Configuration cnf = new Configuration().configure().addAnnotatedClass(UserEntity.class);
+	SessionFactory sf = cnf.buildSessionFactory();
 	public List<UserBean> getAllUser() {
-		return jdbcTemplate.query("select * from User;", new BeanPropertyRowMapper<UserBean>(UserBean.class));
+		
+		Session session = sf.openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from UserEntity");
+		List<UserEntity> lst = query.list();
+		List<UserBean> returnLst = new ArrayList<UserBean>();
+		for(UserEntity list : lst)
+			returnLst.add(list.convertToBean());
+		session.getTransaction().commit();
+		session.close();
+		return returnLst;
+		//return jdbcTemplate.query("select * from User;", new BeanPropertyRowMapper<UserBean>(UserBean.class));
 	}
 
 	public void addUser(UserBean bean) {
-		jdbcTemplate.update("insert into User(MobileNumber,password,UserType) values(?,?,?)", bean.getMobileNumber(),
-				bean.getPassword(), bean.getUserType());
+		//jdbcTemplate.update("insert into User(MobileNumber,password,UserType) values(?,?,?)", bean.getMobileNumber(),
+			//	bean.getPassword(), bean.getUserType());
+		UserEntity u = new UserEntity(bean);
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(u);
+		session.getTransaction().commit();
+		session.close();
 	}
 
 	public void deleteUser(String mobileNumber) {
@@ -29,7 +54,12 @@ public class UserDAO {
 	}
 
 	public UserBean getUser(String mobileNumber) {
-		return jdbcTemplate.queryForObject("select * from User where mobileNumber=?", new BeanPropertyRowMapper<>(UserBean.class), mobileNumber);
+		Session session = sf.openSession();
+		session.beginTransaction();
+		UserEntity user = session.get(UserEntity.class, mobileNumber);
+		session.getTransaction().commit();
+		session.close();
+		return user.convertToBean();
 	}
 
 	public void updateUser(String mobileNumber, UserBean bean) {
