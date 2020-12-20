9bad4623c812722105a541267857f31409e4e3e1
Idris Rupawala <idrisrupawala@gmail.com>
2020-12-20 16:15:10 +0530

    Order

--- src/main/java/com/rms/startup/Controller/OrderedItemsController.java
+++ src/main/java/com/rms/startup/Controller/OrderedItemsController.java
@@ -2,8 +2,6 @@ package com.rms.startup.Controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
